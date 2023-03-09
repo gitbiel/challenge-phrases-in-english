db7d36df07a704320487c0c57a90a478d276653c
Gabriel Mendes <gabriel.f.mend390@gmail.com>
2023-03-09 19:02:48 -0300

    code review

--- script.js
+++ script.js
@@ -18,8 +18,30 @@ function addItem() {
     arr = JSON.parse(localStorage.getItem("meuArr"));
   }
 
-  let novoItem = document.getElementById("name").value;
-  arr.push(novoItem);
+  let novoItem = document.getElementById("name").value.toLowerCase();
+
+  const itemJaExiste = arr.find((item) => item == novoItem);
+  console.log("itemJaExiste", itemJaExiste);
+
+  if (itemJaExiste) {
+  } else {
+    arr.push(novoItem);
+  }
+
   document.getElementById("name").value = "";
   localStorage.meuArr = JSON.stringify(arr);
+}
+
+function showItems() {
+  let resultDIV = document.getElementById("phrasesStayHere");
+  resultDIV.innerHTML = "";
+  if (localStorage.meuArr) {
+    arr = JSON.parse(localStorage.getItem("meuArr"));
+  }
+
+  for (var i in arr) {
+    let p = document.createElement("p");
+    p.innerHTML = arr[i];
+    resultDIV.append(p);
+  }
 }
