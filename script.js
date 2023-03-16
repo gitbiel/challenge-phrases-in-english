50e8e2b8b180a06f7aa0ca51e3105f4e1dcac495
Gabriel Mendes <gabriel.f.mend390@gmail.com>
2023-03-16 16:54:12 -0300

    prevent empty indexes from being sent to localStorage

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 const form = document.querySelector("#form");
+const buttonRegister = document.getElementById("buttonRegister");
+const phrasesTextarea = document.querySelector("#phrase");
+
+buttonRegister.addEventListener("click", function () {
+  disableButton();
+});
+
+function enableButton() {
+  buttonRegister.disabled = false;
+  buttonRegister.style.cursor = "pointer";
+}
+
+function disableButton() {
+  buttonRegister.disabled = true;
+  buttonRegister.style.cursor = "not-allowed";
+}
+
+(function textAreaValidate() {
+  phrasesTextarea.addEventListener("keyup", function (event) {
+    if (phrasesTextarea.value === "") {
+      disableButton();
+    } else {
+      enableButton();
+    }
+  });
+})();
+
+phrasesTextarea.addEventListener("input", () => {
+  phrasesTextarea.value = phrasesTextarea.value
+    ? phrasesTextarea.value.trimStart()
+    : "";
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
