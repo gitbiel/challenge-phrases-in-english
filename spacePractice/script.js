5688ed6a92091f05afd4ce7f77354f72a85de7cd
Isaias Mendes <isaiaszmendes@gmail.com>
2023-03-20 20:57:49 -0300

    add random function

--- spacePractice/script.js
+++ spacePractice/script.js
@@ -1,9 +1,6 @@
-function showPhrase() {
-  const phrasesStayHere = document.getElementById("phrasesStayHere");
-  phrasesStayHere.innerHTML = "";
-  if (localStorage.phraseBank) {
-    arr = JSON.parse(localStorage.getItem("phraseBank"));
-  }
+function randomIntFromInterval(min, max) { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
   for (var i in arr) {
     const item = document.createElement("li");
