ea7f45a6e6938bfd684b915a5959132430c17498
Isaias Mendes <isaiaszmendes@gmail.com>
2023-03-20 21:01:36 -0300

    add func to generate random phrases

--- spacePractice/script.js
+++ spacePractice/script.js
@@ -2,12 +2,11 @@ function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-  for (var i in arr) {
-    const item = document.createElement("li");
-    item.classList.add("paragraph");
-    item.innerHTML = arr[i];
-    phrasesStayHere.append(item);
-  }
+function generateRandomPhrase(list = []) {
+  const max = list.length - 1;
+  const index = randomIntFromInterval(0, max);
+  return list[index]
+}   
 }
 showPhrase();
 console.log(arr);
