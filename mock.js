c4acf79cecc21feea896d4cfdcc4d74440790f9d
Isaias Mendes <isaiaszmendes@gmail.com>
2023-03-20 21:00:09 -0300

    add mock phrases

---
+++ mock.js
@@ -0,0 +1,12 @@
+const mockListPhrases = [
+  "Bob has a cat",
+  "Dad has a cap",
+  "I love this one",
+  "get home on time and have some cake",
+  "after the shower, use the cotton buds to clean your ears",
+  "dancing cheek to cheek is pretty romantic",
+  "the eyes are the doors for the soul",
+  "some people have thin noses, some people have big ones",
+  "keep your mouth shut when your elders talk to you",
+  "when someone is bald, it looks like the forehead gets bigger"
+]
